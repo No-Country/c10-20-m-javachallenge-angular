2bf77f576afeb7b881d649ac8c2a298ddfebc4c0
Tancredi M.S <tancredi620@gmail.com>
2023-04-05 23:38:04 -0300

    add new routes books

--- Backend/src/main/java/com/nocountryproject/Backend/persistence/repository/BookRepository.java
+++ Backend/src/main/java/com/nocountryproject/Backend/persistence/repository/BookRepository.java
@@ -3,5 +3,12 @@ package com.nocountryproject.Backend.persistence.repository;
 import com.nocountryproject.Backend.persistence.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findByTitle(String title);
+    List<Book> findByAuthor(String author);
+
+
 }
