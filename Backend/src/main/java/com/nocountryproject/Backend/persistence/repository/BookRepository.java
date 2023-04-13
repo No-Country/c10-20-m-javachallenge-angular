94c4a28e75a1b09742c996f44a8b41687f807d51
Tancredi M.S <tancredi620@gmail.com>
2023-04-12 22:10:01 -0300

    add findByAuthor repository

--- Backend/src/main/java/com/nocountryproject/Backend/persistence/repository/BookRepository.java
+++ Backend/src/main/java/com/nocountryproject/Backend/persistence/repository/BookRepository.java
@@ -8,6 +8,7 @@ import java.util.List;
 public interface BookRepository extends JpaRepository<Book, Long> {
 
     List<Book> findByTitle(String title);
+    List<Book> findByAuthor(String author);
     List<Book> findByIdCategory(Long id);
 
 
