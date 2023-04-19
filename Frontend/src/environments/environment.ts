22fb69726368f56120a083869243a9f755b2500e
Tancredi M.S <tancredi620@gmail.com>
2023-04-19 17:54:49 -0300

    fix endpoint url

--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,3 +1,3 @@
 export const environment = {
-  APIUrl: 'c10-20-m-javachallenge-angular-production.up.railway.app',
+  APIUrl: 'https://c10-20-m-javachallenge-angular-production.up.railway.app',
 };
